8f4577a7af1bc8360c9d18fe6713e0b84310038a
Enda Mulville <em1716@edge35.doc.ic.ac.uk>
2018-10-17 18:04:23 +0100

    Added input and a try/except for errors. Updated README.

--- API.py
+++ API.py
@@ -1,8 +1,18 @@
 from riotwatcher import RiotWatcher
 
-watcher = RiotWatcher('XXXXXXXXXXXXXXXXXX')
+
+#my api key, XXXXX for privacy
+watcher = RiotWatcher('RGAPI-9e97549d-1ab3-4f7a-8f45-010e2d3760e2')
+
+#user region
 my_region = 'euw1'
 
-me = watcher.summoner.by_name(my_region,'chaos effect')
+summoner_name = raw_input('Enter your summoner name:')
 
-print(me)
+#
+try:
+    me = watcher.summoner.by_name(my_region,summoner_name)
+
+    print(me)
+except:
+    print('An error occured!')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Riot-Api-Project
+
+This is my side project to familiarise myself with Python after mostly programming in C++.
+
+This will mostly exploit the RiotApi to gain access to data from League of Legends
