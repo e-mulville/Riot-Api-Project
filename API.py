01e8a7f67993ec3640c2b975d767a99bac63d5ba
Enda Mulville <endampmulville@gmail.com>
2018-10-17 19:46:09 +0100

    Added Exception handling for the requests.

--- API.py
+++ API.py
@@ -6,16 +6,24 @@ import key
 
 def request_profile(name):
     URL = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name,key.APIkey)
+
     profile = requests.get(URL)
+
+    if profile.json().keys()[0] == 'status':
+        raise Exception("Error code:", profile.json()['status']['status_code'])
+
     return profile.json()
 
 def main():
     summoner_name = raw_input('Please enter summoner name: ')
 
-    profile = request_profile(summoner_name)
+    try:
+        profile = request_profile(summoner_name)
 
-    for key in profile:
-        print key, ": ", profile[key]
+        for key in profile:
+            print key, ": ", profile[key]
+    except Exception as exc:
+        print exc.args[0], exc.args[1]
 
 if __name__ == "__main__":
     main()
