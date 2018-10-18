61f8cbc6ae2e78e2d41e86c2ea9ae2ffa90d4de0
Enda Mulville <endampmulville@gmail.com>
2018-10-18 20:56:05 +0100

    Started on prep for keras/tensor flow. Putting game data in arrays.

--- API.py
+++ API.py
@@ -4,37 +4,52 @@ import numpy as np
 
 import API_requests
 
+import keras
+
 
 def main():
     summoner_name = raw_input('Please enter summoner name: ')
 
+    X = np.zeros((1,101))
+
     try:
         profile = API_requests.request_profile(summoner_name)
 
-        for key in profile:
-            print key, ": ", profile[key]
-
-
-        matchlist = API_requests.request_match_list(summoner_name)
-
-        newest_match_id = matchlist["matches"][0]["gameId"]
-
-        newest_match = API_requests.request_match(newest_match_id)
-
-        print
-
-        for key in range(0,len(newest_match["participantIdentities"])):
-            if newest_match["participantIdentities"][key]["player"]["summonerName"].lower().replace(" ","") == summoner_name.lower().replace(" ",""):
-                summoners_participant_Id = newest_match["participantIdentities"][key]["participantId"]
-
-        for key in range(0,len(newest_match["participants"])):
-            if newest_match["participants"][key]["participantId"] == summoners_participant_Id:
-                print "You got ", newest_match["participants"][key]["stats"]["kills"], " kills in your most recent game."
-
-
-
     except Exception as exc:
         print exc.args[0], exc.args[1]
+
+    for key in profile:
+        print key, ": ", profile[key]
+
+#requesting matchlist for summoner
+    matchlist = API_requests.request_match_list(summoner_name)
+
+#getting their latest match id
+    newest_match_id = matchlist["matches"][0]["gameId"]
+
+#requesting the match data
+    newest_match = API_requests.request_match(newest_match_id)
+
+    #collecting the participant id for the tartget summoner
+    for key in range(0,len(newest_match["participantIdentities"])):
+        if newest_match["participantIdentities"][key]["player"]["summonerName"].lower().replace(" ","") == summoner_name.lower().replace(" ",""):
+            summoners_participant_Id = newest_match["participantIdentities"][key]["participantId"]
+
+    for key in range(0,len(newest_match["participants"])):
+        if newest_match["participants"][key]["participantId"] == summoners_participant_Id:
+            print "You got ", newest_match["participants"][key]["stats"]["kills"], " kills in your most recent game."
+
+
+            #gathering data in a numpy array
+            x = 0
+            for statskey in newest_match["participants"][key]["stats"]:
+                X[0,x] = newest_match["participants"][key]["stats"][statskey]
+                x = x + 1
+
+
+
+    print X
+
 
 if __name__ == "__main__":
     main()
